693dd2f3718a39271f797860fd6d4cdbf46e800c
Madhava Bakkamuntala <Madhavansw@gmail.com>
2026-01-17 21:02:32 +0530

    This is javascript file which having javascript language and i would like to prepare all core related javascript code

---
+++ arr.js
@@ -0,0 +1,24 @@
+
+let name = '' ;
+let Value = name ||'java script';
+console.log(`Name: ${Value}`);
+const fruits = [];
+fruits.push("banana", "apple", "peach");
+let len = fruits.length;
+console.log(`Length: ${len}`);
+let result = fruits.find(x => x == 'apple');
+let index  = fruits.findIndex( x => x == 'apple');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
